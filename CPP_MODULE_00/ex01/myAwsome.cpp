bf8ab3a141a773f50b5320b362056100c44a3d00
Allal EL BAKOURI <95944107+ve-no@users.noreply.github.com>
2023-07-21 17:51:35 +0000

    just starting with this ex01

---
+++ CPP_MODULE_00/ex01/myAwsome.cpp
@@ -0,0 +1,27 @@
+/*
+ * Filename: /Users/ael-bako/1337_cursus/CPP_MODULES/CPP_Module_00/ex01/myAwsome.cpp
+ * Path: /Users/ael-bako/1337_cursus/CPP_MODULES/CPP_Module_00/ex01
+ * Created Date: Thursday, July 21th 2023, 14:20:34 am
+ * Author: Allal El Bakouri
+ */
+
+ #include <iostream>
+
+ class Contact {
+    
+ public:
+    int ADD () {
+
+    }
+    void SEARCH () {
+
+    }
+    void EXIT() {
+
+    }
+
+ }
+
+ class PhoneBook {
+
+ }
