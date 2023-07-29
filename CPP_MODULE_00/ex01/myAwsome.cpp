0a88b0f574b5e39514fc796dbc1c0f3701c416e2
Allal EL BAKOURI <95944107+ve-no@users.noreply.github.com>
2023-07-29 18:57:51 +0000

    just adding some code

--- CPP_MODULE_00/ex01/myAwsome.cpp
+++ CPP_MODULE_00/ex01/myAwsome.cpp
@@ -1,35 +1,90 @@
-/*
- * Filename: /Users/ael-bako/1337_cursus/CPP_MODULES/CPP_Module_00/ex01/myAwsome.cpp
- * Path: /Users/ael-bako/1337_cursus/CPP_MODULES/CPP_Module_00/ex01
- * Created Date: Thursday, July 21th 2023, 14:20:34 am
- * Author: Allal El Bakouri
- */
+
+ /* * * * * * * * * * * * * * * * * * * * * **
+ **    _    __ ______       _   __ ____     **
+ **   | |  / // ____/      / | / // __ \    **
+ **   | | / // __/ ______ /  |/ // / / /    **
+ **   | |/ // /___/_____// /|  // /_/ /     **
+ **   |___//_____/      /_/ |_/ \____/      **
+ ** * * * * * * * * * * * * * * * * * * * * **
+ ** Created Date: July 29th 2023, 17:07     **
+ ** Author: Allal El Bakouri (ael-bako)     **
+ ** Github: ve-no                           **
+ ** * * * * * * * * * * * * * * * * * * * * */
+
 
 #include <iostream>
 #include <string>
 
 class Contact {
    std::string FirstName, LastName, NickName, DarkestSecret, PhoneNumber;
-   Contact(string firstName, string lastName, string nickName, string phoneNumber, string darkestSecret) {
-      FirstName = firstName;
-      LastName = lastName;
-      NickName = nickName;
-      DarkestSecret = darkestSecret;
-      PhoneNumber = phoneNumber;
-   }
- public:
-    int ADD () {
 
+   public:
+      Contact(){};
+      Contact(std::string firstName, std::string lastName, std::string nickName, std::string phoneNumber, std::string darkestSecret) {
+         FirstName = firstName;
+         LastName = lastName;
+         NickName = nickName;
+         DarkestSecret = darkestSecret;
+         PhoneNumber = phoneNumber;
+      }
+       void displayFullInfo() {
+        std::cout << "First Name: " + FirstName << std::endl;
+        std::cout << "Last Name: " + LastName << std::endl;
+        std::cout << "Nickname: " << NickName << std::endl;
+        std::cout << "Phone Number: " << PhoneNumber << std::endl;
+        std::cout << "Darkest Secret: " << DarkestSecret << std::endl;
     }
-    void SEARCH () {
+};
 
-    }
-    void EXIT() {
-
-    }
-}
 
  class PhoneBook {
-   Contact  contacts[8];
+   public:
+    Contact  contacts[8];
+     PhoneBook(){};
+   int ADD(Contact contact);
+
+
+       void Display()
+       {
+         contacts[0].displayFullInfo();
+       }
+   
+ };
+
+   int phonebook::ADD(Contact contact) {
+   int current = 0;
+   if (current < 8)
+      contacts[current++] = contact;
+   else
+      contacts[0] = contact;
+   return (0);
+ };
+
+ int main()
+ {
+   std::string cmd;
+   PhoneBook phonebook;
+   while (1)
+   {
+      std::cout << "ENTER ADD or SEARCH or EXIT\n";
+      std::cin >> cmd;
+      if (cmd == "ADD")
+      {
+         std::string firstName, lastName, nickName, phoneNumber, darkestSecret;
+         std::cout << "Enter the first name\n";
+         std::cin >> firstName;
+         std::cout << "Enter the last name\n";
+         std::cin >> lastName;
+         std::cout << "Enter the nick name\n";
+         std::cin >> nickName;
+         std::cout << "Enter phone number\n";
+         std::cin >> phoneNumber;
+         std::cout << "Enter the Darkest Secret\n";
+         std::cin >> darkestSecret;
+         Contact newContact(firstName, lastName, nickName, phoneNumber, darkestSecret);
+         phonebook.ADD(newContact);
+         phonebook.Display();
+      }
+   }
 
  }
