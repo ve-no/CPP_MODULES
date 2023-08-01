fd90fa4881fb41d73141886a012c08bcf4a4e917
Allal EL BAKOURI <95944107+ve-no@users.noreply.github.com>
2023-08-01 17:25:37 +0000

    just a small header file for contact.cpp

---
+++ MODULE_0/ex01/contact.hpp
@@ -0,0 +1,44 @@
+ /* * * * * * * * * * * * * * * * * * * * * **
+ **    _    __ ______       _   __ ____     **
+ **   | |  / // ____/      / | / // __ \    **
+ **   | | / // __/ ______ /  |/ // / / /    **
+ **   | |/ // /___/_____// /|  // /_/ /     **
+ **   |___//_____/      /_/ |_/ \____/      **
+ ** * * * * * * * * * * * * * * * * * * * * **
+ ** Created Date: July 30th 2023, 13:19     **
+ ** Author: Allal El Bakouri (ael-bako)     **
+ ** Github: ve-no                           **
+ ** * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <cstdio>
+
+class Contact {
+   private:
+      std::string FirstName, LastName, NickName, DarkestSecret, PhoneNumber;
+   public:
+      std::string       ft_subs(std::string& str);
+      void              displayFullInfo();
+      void              displayShortInfo(int index);
+      std::string       getFname (void);
+      std::string       getLname (void);
+      std::string       getNeckName (void);
+      std::string       getPnumber (void);
+      std::string       getDarkestSecret (void);
+      bool              setFname(void);
+      bool              setLname(void);
+      bool              setNeckName(void);
+      bool              setPnumber(void);
+      bool              setDarkestSecret(void);
+      bool              newContact(void);
+};
+bool isAllAlpha(const std::string &str);
+bool isAllDigits(const std::string& str);
+
+#endif
