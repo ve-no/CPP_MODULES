afe7239edf596d8ded122248e6e050ba5c8052c3
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-06 18:55:55 +0100

    header file

---
+++ MODULE_1/ex00/Zombie.hpp
@@ -0,0 +1,21 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+class Zombie {
+    private:
+        std::string name;
+    public:
+        Zombie (std::string _name) {
+            name = _name;
+        }
+        
+        ~Zombie (void) {
+            std::cout << name << " deleted" << std::endl;
+        }
+
+        void    announce (void);
+};
+
+
+#endif
