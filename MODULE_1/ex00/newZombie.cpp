01c89dd53774a7934e5f503a1faad27b2962a115
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-07 18:13:47 +0100

    just finished this file

--- MODULE_1/ex00/newZombie.cpp
+++ MODULE_1/ex00/newZombie.cpp
@@ -1,7 +1,8 @@
 #include "Zombie.hpp"
 
 Zombie* newZombie(std::string name) {
-    Zombie* newZombie = new Zombie(name);
+    Zombie* newZombie = new Zombie;
     
+    newZombie->setName(name);
     return (newZombie);
 }
