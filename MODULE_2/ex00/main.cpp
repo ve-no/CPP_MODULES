874c97776152064965fbd4fcb288495480db06a9
Allal EL BAKOURI <95944107+ve-no@users.noreply.github.com>
2023-08-13 17:32:30 +0000

    ex00 fixed

---
+++ MODULE_2/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "Fixed.hpp"
+
+int main() {
+    Fixed a;
+    Fixed b(a);
+    a= b;
+    a.getRawBits();
+    Fixed c;
+    c = b;
+    a.getRawBits();
+    a.setRawBits(1);
+    b.setRawBits(2);
+    c.setRawBits(3);
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
