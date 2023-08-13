7b1e0566f5913acdfaf47590d354d852fc28104d
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-13 11:53:35 +0100

    all is good

--- MODULE_1/ex01/zombieHorde.cpp
+++ MODULE_1/ex01/zombieHorde.cpp
@@ -1,11 +1,13 @@
 #include "Zombie.hpp"
 
 Zombie* zombieHorde(int N, std::string name) {
-    Zombie* zombieHorde = new Zombie[N];
+
+        Zombie* zombieHorde = NULL;
 
     if (N <= 0)
         std::cout << "no zombie to display" << std::endl;
     else {
+        zombieHorde = new Zombie[N];
         for (int i = 0; i < N; i++) {
             zombieHorde[i].setName(name);
             zombieHorde[i].announce();
