b6764009d267147686ab27d46b2b0dd2b1ccc630
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-17 19:05:52 +0100

    ScavTrap for ex01

---
+++ MODULE_3/ex01/ScavTrap.cpp
@@ -0,0 +1,55 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap()
+{
+	name_ = "default";
+	hitPoints_ = 100;
+	energyPoints_ = 50;
+	attackDamage_ = 20;
+	std::cout << "ScavTrap " << name_ << " created!" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name)
+{
+	name_ = name;
+	hitPoints_ = 100;
+	energyPoints_ = 50;
+	attackDamage_ = 20;
+	std::cout << "ScavTrap " << name_ << " created!" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& copy)
+{
+	this->name_ = copy.name_;
+	this->hitPoints_ = copy.hitPoints_;
+	this->energyPoints_ = copy.energyPoints_;
+	this->attackDamage_ = copy.attackDamage_;
+	std::cout << "ScavTrap " << name_ << " created!" << std::endl;
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& scav)
+{
+	if (this == &scav)
+		return (*this);
+	this->name_ = scav.name_;
+	this->hitPoints_ = scav.hitPoints_;
+	this->energyPoints_ = scav.energyPoints_;
+	this->attackDamage_ = scav.attackDamage_;
+	return (*this);
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << name_ << " destroyed!" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << name_ << " have enterred in Gate keeper mode." << std::endl;
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+	std::cout << "ScavTrap " << name_ << " attack " << target << " ,causing "
+			  << attackDamage_ << " points of damage!" << std::endl;
+}
