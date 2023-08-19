b293f5294a6d02c49626b44f160c9f74bc38b7f0
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-19 19:49:05 +0100

    polymorphism

---
+++ MODULE_4/ex00/Dog.cpp
@@ -0,0 +1,24 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal("Dog") {
+	std::cout << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &copy) : Animal(copy){
+	std::cout << "Dog copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Dog::~Dog() {
+	std::cout << "Dog destructor called" << std::endl;
+}
+
+Dog &Dog::operator=(const Dog &copy) {
+	std::cout << "Dog assignation operator called" << std::endl;
+	this->type = copy.type;
+	return *this;
+}
+
+void Dog::makeSound() const {
+	std::cout << "Dog barks: Woof!" << std::endl;
+}
---
+++ MODULE_4/ex00/main.cpp
@@ -0,0 +1,27 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main() {
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << std::endl;
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+
+	std::cout << std::endl;
+
+	j->makeSound(); //will output the cat sound!
+	i->makeSound();
+
+	std::cout << std::endl;
+
+	meta->makeSound();
+	delete meta;
+	delete j;
+	delete i;
+	return 0;
+}
