ade6bab9548f4799323b56b3b5efce749315cf84
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-20 20:03:47 +0100

    add new class caled AAnimal

---
+++ MODULE_4/ex02/AAnimal.cpp
@@ -0,0 +1,35 @@
+
+#include "AAnimal.hpp"
+
+
+AAnimal::AAnimal() : type("AAnimal") {
+	std::cout << "AAnimal default constructor called" << std::endl;
+}
+
+AAnimal::AAnimal(const AAnimal &copy) {
+	std::cout << "AAnimal copy constructor called" << std::endl;
+	*this = copy;
+}
+
+AAnimal::~AAnimal() {
+	std::cout << "AAnimal destructor called" << std::endl;
+}
+
+AAnimal &AAnimal::operator=(const AAnimal &copy) {
+	std::cout << "AAnimal assignation operator called" << std::endl;
+	this->type = copy.type;
+	return *this;
+}
+
+std::string AAnimal::getType() const {
+	return this->type;
+}
+
+void AAnimal::makeSound() const {
+	std::cout << "AAnimal makes a sound" << std::endl;
+}
+
+
+
+
+
