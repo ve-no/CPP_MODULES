90759d4a03b854872b4f67c18cacc98e91224bfb
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-22 20:24:44 +0100

    add the missing part of my code

---
+++ MODULE_4/ex02/WrongAnimal.hpp
@@ -0,0 +1,20 @@
+#ifndef WRONGANIMAL_HPP
+# define WRONGANIMAL_HPP
+
+# include <iostream>
+
+class WrongAnimal
+{
+	protected:
+		std::string type;
+	public:
+		WrongAnimal();
+		WrongAnimal(std::string type);
+		WrongAnimal(const WrongAnimal &copy);
+		WrongAnimal &operator=(const WrongAnimal &copy);
+		virtual ~WrongAnimal();
+		void makeSound() const;
+		std::string getType() const;
+};
+
+#endif
