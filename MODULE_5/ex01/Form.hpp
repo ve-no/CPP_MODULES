7080f688d39a2802efb5bf57848e8622e0951880
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-31 19:12:15 +0100

    Form

---
+++ MODULE_5/ex01/Form.hpp
@@ -0,0 +1,41 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form{
+private:
+	const std::string _name;
+	bool _isSigned;
+	const int _gradeToSign;
+	const int _gradeToExecute;
+
+public:
+	Form(const std::string name, const int gradeToSign ,const int gradeToExecute);
+	~Form();
+	const std::string getName() const;
+	bool isSigned() const;
+	int getGradeToSign() const;
+	int getGradeToExecute() const;
+
+	void beSigned(const Bureaucrat &Bureaucrat);
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Form& form);
+
+#endif
