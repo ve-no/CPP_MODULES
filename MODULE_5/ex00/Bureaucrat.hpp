c7e14f4287694eae57b3b47ebf7d4225b862efc5
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-08-31 19:11:35 +0100

    Bureaucrat

---
+++ MODULE_5/ex00/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		int _grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name, int grade);
+		Bureaucrat(const Bureaucrat &copy);
+		~Bureaucrat();
+
+		Bureaucrat &operator=(const Bureaucrat &copy);
+
+		const std::string getName() const;
+		int getGrade() const;
+
+		void incrementGrade();
+		void decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+
+
+
+#endif
