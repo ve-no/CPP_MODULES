e66e559bf3d7afe7540c68d055896195cf511947
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-05 14:32:57 +0100

    all is goood

---
+++ MODULE_6/ex02/dynamic_cast.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+class Base {
+public:
+	virtual ~Base() {}
+};
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+Base *generate();
+
+void identify(Base* p);
+
+void identify(Base& p);
