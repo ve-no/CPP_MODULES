526fb5dd1e653df6efa9f63b71691905438f4ea4
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-05 14:31:34 +0100

    Main file

--- MODULE_5/ex01/main.cpp
+++ MODULE_5/ex01/main.cpp
@@ -6,11 +6,11 @@ int main()
 	try
 	{
 		Bureaucrat b("Bureaucrat", 1);
-		std::cout << b << std::endl;
+		// std::cout << b << std::endl;
 		Form f("Form", 1, 1);
-		std::cout << f << std::endl;
+		// std::cout << f << std::endl;
 		b.signForm(f);
-		std::cout << f << std::endl;
+		// std::cout << f << std::endl;
 	}
 	catch (std::exception &e)
 	{
@@ -19,14 +19,14 @@ int main()
 	try
 	{
 		Bureaucrat b("Bureaucrat", 15);
-		std::cout << b << std::endl;
+		// std::cout << b << std::endl;
 		Form f("Form", 1, 10);
-		std::cout << f << std::endl;
+		// std::cout << f << std::endl;
 		b.signForm(f);
-		std::cout << f << std::endl;
+		// std::cout << f << std::endl;
 		Form ff("Form", 15, 1);
 		b.signForm(ff);
-		std::cout << ff << std::endl;
+		// std::cout << ff << std::endl;
 	}
 	catch (std::exception &e)
 	{
