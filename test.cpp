8cff47b13c30f0e90cc784346872cf978d3f5253
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-14 17:38:00 +0100

    All is good

--- test.cpp
+++ test.cpp
@@ -1,36 +1,49 @@
-#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
-class Animal {
+class Span {
+private:
+    std::vector<int> numbers;
+
 public:
-    virtual void make_sound() const {
-        std::cout << "Animal makes a sound" << std::endl;
+    // Constructor to initialize the Span with a maximum size.
+    Span(int size) {
+        numbers.reserve(size);
+    }
+
+    // Function to add a single number to the Span.
+    void addNumber(int num) {
+        if (numbers.size() < numbers.capacity()) {
+            numbers.push_back(num);
+        }
+    }
+
+    // Function to add a range of numbers using iterators.
+    template <typename InputIterator>
+    void addRange(InputIterator begin, InputIterator end) {
+        // Calculate the number of elements to add.
+        size_t numToAdd = std::distance(begin, end);
+
+        // Ensure that adding these elements won't exceed the capacity.
+        if (numbers.size() + numToAdd <= numbers.capacity()) {
+            // Use std::copy to add elements from the range to the vector.
+            std::copy(begin, end, std::back_inserter(numbers));
+        }
+        // You may want to handle an error condition if the capacity is exceeded.
     }
 };
-
-class Dog : public Animal {
-public:
-    void make_sound() const {
-        std::cout << "Dog barks: Woof!" << std::endl;
-    }
-};
-
-class Cat : public Animal {
-public:
-    void make_sound() const{
-        std::cout << "Cat meows: Meow!" << std::endl;
-    }
-};
-
-void animal_sounds(const Animal* animal) {
-    animal->make_sound();
-}
 
 int main() {
-    Dog* dog = new Dog();
-    Cat *cat = new Cat();
+    Span span(10); // Create a Span with a maximum capacity of 10.
 
-    animal_sounds(dog);  // Output: Dog barks: Woof!
-    animal_sounds(cat);  // Output: Cat meows: Meow!
+    std::vector<int> numbersToAdd = {1, 2, 3, 4, 5};
+
+    // Add a range of numbers to the Span.
+    span.addRange(numbersToAdd.begin(), numbersToAdd.end());
+
+    // Add a single number to the Span.
+    span.addNumber(6);
 
     return 0;
 }
