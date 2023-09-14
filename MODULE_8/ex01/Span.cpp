9c4e6d3418f9c30564031f0777d07af73fb701bd
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2023-09-14 17:37:41 +0100

    All is good

--- MODULE_8/ex01/Span.cpp
+++ MODULE_8/ex01/Span.cpp
@@ -1,26 +1,54 @@
 #include "Span.hpp"
+#include <climits>
 
 
-Span::Span(unsigned int N): n(N) {}
+Span::Span(unsigned int N) {
+	nums.reserve(N);
+}
+
+Span::Span(const Span &copy)
+{
+	*this = copy;
+}
+
+Span &Span::operator=(const Span &copy)
+{
+	if (this != &copy) {
+		this->nums = copy.nums;
+	}
+	return *this;
+}
+
+Span::~Span() {}
 
 void Span::addNumber(int num)
 {
-	if (n == nums.size())
+	if (nums.size() < nums.capacity())
+		nums.push_back(num);
+	else
 		throw std::runtime_error("the vector is full");
-	nums.push_back(num);
 }
 
-int Span::shortestSpan()
+long Span::shortestSpan() const
 {
-	return 0;
+	std::vector<int> vec = nums;
+	long short_span = INT_MAX;
+	if (vec.size() < 2)
+		throw std::runtime_error("there is less then 2 element in the container");
+	std::sort(vec.begin(), vec.end());
+	for (int i = 1; i < (int)vec.size(); i++) {
+		if ((static_cast <long> (vec.at(i)) - vec.at(i - 1)) < short_span)
+			short_span = static_cast <long> (vec.at(i)) - vec.at(i - 1);
+	}
+	return short_span;
 }
 
-int Span::longestSpan()
+long Span::longestSpan() const
 {
-	std::sort(nums.begin(), nums.end());
-	return nums.end() - nums.begin();
+	std::vector<int> vec = nums;
+	if (vec.size() < 2)
+		throw std::runtime_error("there is less then 2 element in the container");
+	std::sort(vec.begin(), vec.end());
+	return static_cast <long> (vec.back()) - vec.front();
 }
 
-void Span::addNumbers(const std::vector<int> &nums)
-{
-}
