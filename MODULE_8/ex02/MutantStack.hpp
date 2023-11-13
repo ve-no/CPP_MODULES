804f8740fb04cf2e47790ffa645df964606efe15
ve-no <allal.elbakourii@gmail.com>
2023-11-13 11:00:06 +0100

    Update MutantStack to support custom container types

--- MODULE_8/ex02/MutantStack.hpp
+++ MODULE_8/ex02/MutantStack.hpp
@@ -1,26 +1,29 @@
+#pragma once
 #include <vector>
 #include <stack>
+#include <map>
+#include <deque>
 #include <iostream>
 
-template <typename T>
-class MutantStack : public std::stack<T>
+template <typename T, typename container = std::deque<T> >
+class MutantStack : public std::stack<T, container>
 {
 public:
-	MutantStack() : std::stack<T>() {}
-	MutantStack(MutantStack const &other) : std::stack<T>(other) {}
-	MutantStack &operator=(MutantStack const &other)
+	MutantStack() : std::stack<T, container>() {}
+	MutantStack(MutantStack<T, container> const &other) : std::stack<T, container>(other) {}
+	MutantStack<T, container> &operator=(MutantStack<T, container> const &other)
 	{
 		if (this != &other)
-			std::stack<T>::operator=(other);
+			std::stack<T, container>::operator=(other);
 		return *this;
 	}
 	virtual ~MutantStack() {}
 
-	typedef typename std::stack<T>::container_type::iterator iterator;
-	iterator begin() { return std::stack<T>::c.begin(); }
-	iterator end() { return std::stack<T>::c.end(); }
+	typedef typename container::iterator iterator;
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
 
-	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-	const_iterator begin() const { return std::stack<T>::c.begin(); }
-	const_iterator end() const { return std::stack<T>::c.end(); }
+	typedef typename container::const_iterator const_iterator;
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
 };
