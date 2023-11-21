dece099fb98c68af65cc99ab5323be80b3e16ee8
ve-no <allal.elbakourii@gmail.com>
2023-11-21 18:57:59 +0100

    Refactor BitcoinExchange initialization and error handling

--- MODULE_9/ex00/main.cpp
+++ MODULE_9/ex00/main.cpp
@@ -1,13 +1,18 @@
 #include "BitcoinExchange.hpp"
 
 int main () {
-
-    BitcoinExchange exchange;
-    exchange.readData("data.csv");
-    std::map<std::string, double> rates = exchange.getRates();
-    std::map<std::string, double>::iterator it = rates.begin();
-    while (it != rates.end()) {
-        std::cout << it->first << " " << std::endl;
-        it++;
+    try {
+    BitcoinExchange b("file.csv", "data.csv");
+    // std::map<std::string, double> m = b.getRates();
+    // std::map<std::string, double>::iterator it = m.begin();
+    // while (it != m.end()) {
+    //     std::cout << it->first << " " << it->second << std::endl;
+    //     it++;
+    // }
+    // b.readData("data.csv");
     }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
 }
